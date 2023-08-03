09f738cc0be194beda1dafac193da2b2b3d1c514
rahul07sharmars <rahul07sharmars@gmail.com>
2023-08-03 16:08:03 +0530

    current location and min and maxzoom

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_gl/mapbox_gl.dart';
+// import 'package:latlong2/latlong.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -17,55 +19,33 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: MapboxMap(
+        minMaxZoomPreference: MinMaxZoomPreference(7,13),
+        styleString: 'mapbox://styles/rahul07sharma/clktkvaqb00co01o2dntb8xqk',
+        // 'https://api.mapbox.com/styles/v1/rahul07sharma/clktkvaqb00co01o2dntb8xqk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmFodWwwN3NoYXJtYSIsImEiOiJjbGtzYmN6NnMwMDF6M2hvYTRvb2FwdDI2In0.7tSwDDTzzWIx1VhtYbfvNQ',
+        myLocationEnabled:true,
+        accessToken: 'sk.eyJ1IjoicmFodWwwN3NoYXJtYSIsImEiOiJjbGt1bzMyNnYwMGRyM2xwZHJuMXF2OTMzIn0.hojM5Wqzbm-fXmMzbSHlag',
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(12.972442, 77.580643),
+          zoom:10,
+          tilt:60,
         ),
+          annotationOrder:[AnnotationType.symbol,
+            ]
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // MapboxOverlay(
+        // controller: MapboxOverlayController(),
+        // options: MapboxMapOptions(
+        //   style: Style.dark,
+        //   camera: new CameraPosition(
+        //       target: LatLng(lat: 52.376316, lng: 4.897801),
+        //       zoom: 15.0,
+        //       bearing: 0.0,
+        //       tilt: 0.0),
+        // ),
+
     );
   }
 }
+
